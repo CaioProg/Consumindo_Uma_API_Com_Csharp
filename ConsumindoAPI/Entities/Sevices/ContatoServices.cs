30c88b27d1abab53fef54d71eba53cba793f9008
CaioProg <caioprog18@gmail.com>
2023-01-21 23:34:04 -0300

    Consumindo uma API GET

---
+++ ConsumindoAPI/Entities/Sevices/ContatoServices.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumindoAPI.Entities.Sevices
+{
+    internal class ContatoServices
+    {
+        public async Task<Contato> Integracao(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://localhost:7041/Contato/{id}");
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            // Converte o Json recebido para o objeto Contato
+            Contato jsonObject = JsonConvert.DeserializeObject<Contato>(jsonString);
+
+            if(jsonObject.Nome != null)
+            {
+                return jsonObject;
+            }
+            else
+            {
+                return new Contato
+                {
+                    Verificacao = true
+                };
+            }
+        }
+    }
+}
---
+++ ConsumindoAPI/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsumindoAPI.Entities;
+using ConsumindoAPI.Entities.Sevices;
+
+namespace ConsumindoAPI
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Sistema para consumir API");
+
+            Console.WriteLine("Por favor, infome o id do contato: ");
+            int id = int.Parse(Console.ReadLine());
+
+            ContatoServices contatoServices = new ContatoServices();
+            Contato contatoEncontrado = await contatoServices.Integracao(id);
+
+            if(!contatoEncontrado.Verificacao)
+            {
+                Console.WriteLine("Contato encontrado");
+
+                Console.WriteLine($"Nome: {contatoEncontrado.Nome}");
+                Console.WriteLine($"Telefone: {contatoEncontrado.Telefone}");
+                Console.WriteLine($"Ativo: {contatoEncontrado.Ativo}");
+                Console.WriteLine($"Verificação: {contatoEncontrado.Verificacao}");
+            }
+            else
+            {
+                Console.WriteLine("Aluno não encontrado!");
+            }
+        }
+    }
+}
